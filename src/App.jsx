29404427736fb1195f94e051f493e20654a119bc
Charlie Mickey <118931297+charlesmickey222@users.noreply.github.com>
2023-07-23 13:57:42 -0500

    add image display element w placeholder image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import FileOpener from "./FileOpener";
+import ImageDisplay from "./ImageDisplay";
 
 function App() {
 
   return (
     <div className="container">
       <FileOpener/>
+      <ImageDisplay/>
     </div>
   );
 }
--- src/ImageDisplay.jsx
+++ src/ImageDisplay.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import "./ImageDisplay.css";
+
+
 const ImageDisplay = () => {
+  const [imageFilePath, setImageFilePath] = useState('placeholder-image.png');
   return ( 
     <div id="DisplayContainer">
-      <img className="Display"/>
+      <img className="Display" src={imageFilePath}/>
     </div>
   );
 }
